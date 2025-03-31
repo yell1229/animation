d8bf4d267c94a41a915dd4c14a6606c985ab1364
kim <yellsdesign@naver.com>
2025-03-31 23:00:53 +0900

    gsap

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # animation
 
 ## Link
-- Group Effect
-[https://cdnjs.com/libraries/web-animations](https://cdnjs.com/libraries/web-animations)
+- Group Effect<br>
+[https://cdnjs.com/libraries/web-animations](https://cdnjs.com/libraries/web-animations)<br>
+
+-gsap<br>
+[https://gsap.com/](https://gsap.com/docs/v3/Installation?tab=cdn&module=esm&method=private+registry&tier=free&club=false&require=false&trial=true&plugins=ScrollTrigger)<br>
--- test-animation/src/components/Scroll01.jsx
+++ test-animation/src/components/Scroll01.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import '../scss/section.scss';
 
 export default function Scroll01() {
     return (
-        <div>
-            scroll
+        <div className='section2'>
+            <div className="container">
+                <div className="box1">Projects</div>
+                <div className="box2">
+                    <div className="col">
+                        <ul>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <ul>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <ul>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <ul>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                            <li><img src="https://placehold.co/300x400" alt="" /></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
